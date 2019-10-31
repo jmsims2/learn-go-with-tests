616857214b885422715ec71109a7f448eeea218d
jmsims2 <jmsims2@gmail.com>
2019-10-30 22:40:07 -0600

    refactor

--- hello/hello.go
+++ hello/hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const greeting = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name + "!"
+	if name == "" {
+		name = "World"
+	}
+	return greeting + name + "!"
 }
 
 func main() {
