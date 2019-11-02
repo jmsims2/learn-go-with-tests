087fc1990282cdaa13ce9c903619ca86655b2947
jmsims2 <jmsims2@gmail.com>
2019-11-01 22:59:52 -0600

    more slicing

--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -1,8 +1,18 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
@@ -12,5 +22,23 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d want %d given %v", got, want, numbers)
 		}
+	})
+
+	t.Run("sum multiple slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("sum all tails", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("sum all tails with an empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{})
+		want := []int{2, 0}
+		checkSums(t, got, want)
 	})
 }
