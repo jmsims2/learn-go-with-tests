edddbad90f5fadb4956ba8fc17acbeea98f836d7
jmsims2 <jmsims2@gmail.com>
2019-11-03 20:51:51 -0700

    structs, methods and interfaces

---
+++ structs-methods-interfaces/shapes_test.go
@@ -0,0 +1,46 @@
+package main
+
+import "testing"
+
+func TestShapes(t *testing.T) {
+
+	checkResults := func(t *testing.T, got, want float64) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+
+	// checkArea := func(t *testing.T, shape Shape, want float64) {
+	// 	t.Helper()
+	// 	got := shape.Area()
+	// 	if got != want {
+	// 		t.Errorf("got %g want %g", got, want)
+	// 	}
+	// }
+
+	t.Run("test perimeter rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Perimeter()
+		want := 40.0
+
+		checkResults(t, got, want)
+	})
+
+	t.Run("test Area", func(t *testing.T) {
+		areaTests := []struct {
+			shape Shape
+			want  float64
+		}{
+			{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+			{shape: Circle{Radius: 10}, want: 314.1592653589793},
+			{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+		}
+		for _, tt := range areaTests {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			}
+		}
+	})
+}
